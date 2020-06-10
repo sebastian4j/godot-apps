8f281ef96edc3c51ca29405774b34b612aa3562c
Sebastián Ávila A <sebastian4j@gmail.com>
2020-06-10 12:21:57 -0400

    ejemplo de señales personalizadas

--- timer_conector_codigo/raiz2d.gd
+++ timer_conector_codigo/raiz2d.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
+signal my_signal
+signal my_signal2(value, other_value)
+
 func _ready():
+	emit_signal("my_signal2", true, 42)
+	emit_signal("my_signal")
 	$Timer.connect("timeout", self, "cambiar_sprite")
 	$Timer.start()
 
